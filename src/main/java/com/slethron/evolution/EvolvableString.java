ec83ac2a7568f1639eadc0185d9bcb0444294ee8
Chris Mims <cmimw@allstate.com>
2018-03-17 01:44:26 -0400

    Abstract methods of algorithm into new abstract class - the new abstract class has a type parameter

--- src/main/java/com/slethron/evolution/EvolvableString.java
+++ src/main/java/com/slethron/evolution/EvolvableString.java
@@ -1,57 +1,19 @@
 package com.slethron.evolution;
 
-import java.math.BigInteger;
 import java.util.Random;
 
-public class StringEvolver {
+public class EvolvableString extends Evolvable<String> {
     StringBuffer sb;
     Random random;
 
-    public StringEvolver() {
-        random = new Random();
+    public EvolvableString(String source) {
+        super(source);
         sb = new StringBuffer();
+        random = new Random();
     }
 
-    public void solve(String _source, String _target) throws IllegalArgumentException {
-        String source = _source;
-        String target = _target;
-        if (source.length() != target.length() || source.length() == 0 || target.length() == 0) {
-            throw new IllegalArgumentException();
-        }
-
-        double fitval = fitness(source, target);
-
-        BigInteger i = BigInteger.ZERO;
-        while (true) {
-            i = i.add(BigInteger.ONE);
-            String m = mutate(source);
-            double fitval_m = fitness(m, target);
-
-            boolean accepted = false;
-            if (fitval_m < fitval) {
-                fitval = fitval_m;
-                source = m;
-                accepted = true;
-            }
-
-            sb.delete(0, sb.length())
-                    .append("index ")
-                    .append(i)
-                    .append(" : fitval_m ")
-                    .append(fitval_m)
-                    .append(" : m ")
-                    .append(m)
-                    .append(accepted ? " : accepted" : " : rejected");
-
-            System.out.println(sb.toString());
-
-            if (fitval == 0) {
-                break;
-            }
-        }
-    }
-
-    private double fitness(String source, String target) {
+    @Override
+    Double fitness(String source, String target) {
         double fitval = 0;
         for (int i = 0; i < source.length(); i++) {
             fitval += Math.abs(target.charAt(i) - source.charAt(i));
@@ -60,7 +22,8 @@ public class StringEvolver {
         return fitval;
     }
 
-    private String mutate(String source) {
+    @Override
+    String mutate(String source) {
         String m = source;
         int i = random.nextInt(m.length());
         int flip = random.nextBoolean() ? 1 : -1;
@@ -83,7 +46,7 @@ public class StringEvolver {
         String source = "!dlroW ,olleH";
         String target = "Hello, World!";
 
-        StringEvolver se = new StringEvolver();
-        se.solve(source, target);
+        EvolvableString se = new EvolvableString(source);
+        se.solve(target);
     }
 }
